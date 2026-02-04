33119d77e5777d7578f9b7f033850421bbaab9f1
Kerr Beeldens <kerrbeeldens@outlook.com>
2026-02-04 15:34:22 +0100

    Add whoami command using fetch

--- stateMachine/WelcomeState.js
+++ stateMachine/WelcomeState.js
@@ -1,3 +1,4 @@
+import { getStudentData } from "../display/FDND/Fdnd.js";
 import { BaseState } from "./BaseState.js";
 
 export class WelcomeState extends BaseState {
@@ -6,10 +7,13 @@ export class WelcomeState extends BaseState {
         super(context);
         // clear the terminal (hard reset)
         this.context.terminal.clear();
-        this.context.terminal.showLine("Hello, World!");
+        this.context.terminal.showLine("Kerrminal");
+        this.context.terminal.showLine("reset - reset this console");
+        this.context.terminal.showLine("echo - echo your command");
+        this.context.terminal.showLine("whoami - show personal information");
     }
 
-    handleCommand(command) {
+    async handleCommand(command) {
         let tokens = command.split(" ");
 
         switch (tokens[0]) {
@@ -19,7 +23,17 @@ export class WelcomeState extends BaseState {
             case "echo": // Simply echo user
                 this.context.terminal.showLine(tokens.slice(1).join(" "));
                 break;
-
+            case "whoami": // print personal data   
+                let personalData = await getStudentData(302);
+                if (personalData) {
+                    console.log(personalData.name);
+                    this.context.terminal.showLine("Naam: " + personalData.name);
+                    this.context.terminal.showLine("Bijnaam: " + personalData.nickname);
+                    this.context.terminal.showLine("Verjaardag: " + personalData.birthdate);
+                } else {
+                    this.context.terminal.showLine("Failed to fetch personal data.");
+                }
+                break;
             default: // Unkown command, check if global command
                 super.handleCommand(command)
         }
