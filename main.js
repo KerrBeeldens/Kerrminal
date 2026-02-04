7b0dabde56ae96d50a2ca358ed3f7cb15171310f
Kerr Beeldens <kerrbeeldens@outlook.com>
2026-02-04 12:14:09 +0100

    Add simple interaction

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+let terminalWindow = document.querySelector(".terminal");
+let inputline = document.querySelector("#input-line-field");
 
+terminalWindow.addEventListener("click", (e) => {
+    inputline.select();
+});
+
+inputline.addEventListener('keyup', function (e) {
+    if (e.key === 'Enter' && inputline.value !== "") {
+        inputline.value = "";
+        // parse text
+    }
+
+    //inputline.value = "";
+    console.log(e.key);
+});
