342bafed3742972b5f6df416d1569d61a7a7ac04
Kerr Beeldens <kerrbeeldens@outlook.com>
2026-02-10 11:01:13 +0100

    Refactor main.js

--- display/startup.js
+++
@@ -1,5 +0,0 @@
-let bootupScreen = document.querySelector(".os-bootup");
-bootupScreen.addEventListener("click", () => {
-  bootupScreen.classList.add("launching");
-  console.log("test");
-});
--- main.js
+++ main.js
@@ -3,66 +3,65 @@ import { getStudentData } from "../FDND/Fdnd.js";
 
 let context = new Context();
 
-let bootupScreen = document.querySelector(".os-bootup");
-bootupScreen.addEventListener("click", () => {
-  bootupScreen.classList.add("open");
+/* === Lock Screen === */
+const lockScreen = document.querySelector(".lock-screen");
+
+// Open the lockscreen on click
+lockScreen.addEventListener("click", () => {
+  lockScreen.classList.add("lock-screen-unlocked");
 });
 
-const loginForm = document.querySelector(".os-login-form");
+/* === Login Screen === */
+const loginForm = document.querySelector(".login-form");
 const loginField = document.querySelector("#login-field");
 
-async function onLogin(e) {
-  e.preventDefault();
+// Reset validity on input
+loginField.addEventListener("input", () => {
+  loginField.setCustomValidity("");
+});
 
+// On login attempt, retrieve and display student data
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
   loginField.setCustomValidity("");
 
   let formData = new FormData(loginForm);
   let id = formData.get("login");
 
-  console.log("Attempting login with ID:", id);
-
+  // Retrieve student data
   let studentData;
 
   try {
     studentData = await getStudentData(id);
-
-    if (!studentData.data) {
-      throw new Error("Invalid ID or missing data");
-    }
+    if (!studentData.data) throw new Error("Invalid ID or missing data");
   } catch (err) {
     loginField.setCustomValidity("Invalid ID");
     loginField.reportValidity();
-    console.log("all good");
     return;
   }
 
-  let window = document.querySelector(".os-login");
+  // Retrieved valid data, build login window
+  const window = document.querySelector(".login-screen");
   window.innerHTML = "";
 
-  let pfp = document.createElement("img");
-  pfp.classList.add("pfp");
-  pfp.src = studentData.data.avatar || "resources/pfp.png";
+  const profilePicture = document.createElement("img");
+  profilePicture.classList.add("profile-picture");
+  profilePicture.src = studentData.data.avatar || "resources/pfp.png"; // TODO on 404
 
-  let welcomeText = document.createElement("p");
+  const welcomeText = document.createElement("p");
   welcomeText.classList.add("welcome-message");
   welcomeText.textContent = `Welcome ${studentData.data.name}!`;
 
-  let loader = document.createElement("div");
+  const loader = document.createElement("div");
   loader.classList.add("loader");
 
-  window.append(pfp, welcomeText, loader);
-  window.classList.add("remove");
-}
-
-loginForm.addEventListener("submit", onLogin);
-
-loginField.addEventListener("input", () => {
-  loginField.setCustomValidity("");
+  window.append(profilePicture, welcomeText, loader);
+  window.classList.add("login-screen-remove");
 });
 
+/* === Taskbar === */
+
 // Source - https://stackoverflow.com/a/39418437
-// Posted by Pranav C Balan, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-02-09, License - CC BY-SA 4.0
 const time = document.querySelector(".time");
 
 function getTime() {
@@ -72,4 +71,5 @@ function getTime() {
   time.textContent = ("0" + h).slice(-2) + ":" + ("0" + m).slice(-2);
 }
 
+// Update the time every second
 setInterval(getTime, 1000);
